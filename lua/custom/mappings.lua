13069c9cd14e850025f2717146bf8d576f2d0162
sumarokov-vp <sumarokov.vp@gmail.com>
2023-09-13 22:28:31 +0600

    123

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -30,18 +30,6 @@ M.dap_python = {
     }
   }
 }
---
--- M.copilot = {
---   i = {
---     ["<C-a>"] = {
---       function()
---         vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
---       end,
---       "Copilot Accept",
---        {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
---       }
---   }
--- }
 
 M.lazygit = {
   plugin = true,
